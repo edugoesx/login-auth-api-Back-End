4e9f113a98d41d8372afc91a945a3b14b7b39d73
Eduardo <edu.goes10@gmail.com>
2026-01-04 19:06:25 -0300

    environment variables

--- src/main/java/com/example/login_auth_api/infra/security/TokenService.java
+++ src/main/java/com/example/login_auth_api/infra/security/TokenService.java
@@ -14,7 +14,7 @@ import java.time.ZoneOffset;
 
 @Service
 public class TokenService {
-    @Value("${api.securiy.token.secret}")
+    @Value("${api.security.token.secret}")
     private String secret;
     public String generateToken(User user){
         try {
